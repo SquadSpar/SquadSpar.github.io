c9e90d078e223e1737ca93895e07354c49f2993c
Julian Phillips <julian.p@me.com>
2019-12-22 16:37:00 -0500

    feat: Imported BackgroundWithOverlay on Home, made buttons StyledButtons

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -3,50 +3,48 @@ import { Link } from "react-router-dom";
 
 // Project Components
 import StyledButton from "../Components/StyledButton";
+import BackgroundWithOverlay from "../Components/BackgroundWithOverlay";
 
 import "../stylesheets/home.scss";
 // import '../../stylesheets/home.scss'
-import axios from 'axios'
+import axios from "axios";
 class Home extends Component {
-
   createRoom = () => {
-    //axios.post('/createRoom').then(res=>{ //Get endpoint from Neil 
-      this.props.history.push('/newRoomPage')
+    //axios.post('/createRoom').then(res=>{ //Get endpoint from Neil
+    this.props.history.push("/newRoomPage");
     //})
-  } 
+  };
 
   joinRoom = () => {
-    let roomNumber = prompt('whats your room number?')
-    console.log(roomNumber)
-    //axios.post('/joinRoom').then(res=>{ //Get endpoint from Neil 
-      this.props.history.push(`/room/${roomNumber}`)
-    //})    
-
-  }
+    let roomNumber = prompt("whats your room number?");
+    console.log(roomNumber);
+    //axios.post('/joinRoom').then(res=>{ //Get endpoint from Neil
+    this.props.history.push(`/room/${roomNumber}`);
+    //})
+  };
 
   render() {
-    console.log(this)
+    console.log(this);
     return (
       <div className="home">
-        {/* <StyledButton title="Student" />
-        <button>TEACHER</button> */}
-        <img
-          src="../../images/home.png"
-          className="home-background"
-          alt="background"
-        />
+        <BackgroundWithOverlay imgUrl="home.png" alpha=".6" />
         <h1 className="welcome">Welcome to Squad Spur!</h1>
         <p>Pick your role</p>
         <Link to="/">
           <img src="../../images/student.png" alt="student icon" />
         </Link>
-        <button onClick={this.joinRoom} >STUDENT | Join Room</button>
-        <p>Student</p>
+        <StyledButton
+          title="STUDENT | Join Room"
+          onClickFunction={this.joinRoom}
+        />
+
         <Link to="/">
           <img src="../../images/teacher.png" alt="teacher icon" />
         </Link>
-        <button onClick={this.createRoom}>TEACHER | Create Room</button>
-        <p>Teacher</p>
+        <StyledButton
+          title="TEACHER | Create Room"
+          onClickFunction={this.createRoom}
+        />
       </div>
     );
   }
