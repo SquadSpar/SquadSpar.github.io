57e2554f994350a074976ddd02f857493402bfb7
Vitalii Agapov <vitaly.agapov@outlook.com>
2019-12-22 16:06:52 -0500

    added room and some changes at home

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,12 +1,29 @@
 import React, { Component } from 'react'
 // import '../../stylesheets/home.scss'
-
+import axios from 'axios'
 class Home extends Component {
+
+  createRoom = () => {
+    //axios.post('/createRoom').then(res=>{ //Get endpoint from Neil 
+      this.props.history.push('/newRoomPage')
+    //})
+  } 
+
+  joinRoom = () => {
+    let roomNumber = prompt('whats your room number?')
+    console.log(roomNumber)
+    //axios.post('/joinRoom').then(res=>{ //Get endpoint from Neil 
+      this.props.history.push(`/room/${roomNumber}`)
+    //})    
+
+  }
+
   render() {
+    console.log(this)
     return (
       <div className="home">
-        <button>STUDENT</button>
-        <button>TEACHER</button>
+        <button onClick={this.joinRoom} >STUDENT | Join Room</button>
+        <button onClick={this.createRoom}>TEACHER | Create Room</button>
       </div>
     )
   }
