a57a5bd68d3a8c4bce4be51b8b61c538411cccb3
Elba Chimilio <elbachimilio@gmail.com>
2019-12-22 13:51:18 -0500

    Added to .gitignore and created seperate folders for components, pages, etc

---
+++ src/containers/Home.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+// import '../../stylesheets/home.scss'
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="home">
+        <button>Instructor</button>
+        <button>Student</button>
+      </div>
+    )
+  }
+}
+
+export default Home;
