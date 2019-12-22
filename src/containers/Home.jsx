a2711bfa41308fe8617d9b05ea5488a4a5de9d3a
Elba Chimilio <elbachimilio@gmail.com>
2019-12-22 15:35:48 -0500

    Working on designing home page

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -5,7 +5,12 @@ class Home extends Component {
   render() {
     return (
       <div className="home">
+        <img src="../../images/home.png" className="home-background"/>
+        <h1 className="welcome">Welcome to Squad Spur!</h1>
+        <p>Pick your role</p>
+        <img src="../../images/student.png"/>
         <button>STUDENT</button>
+        <img src="../../images/teacher.png"/>
         <button>TEACHER</button>
       </div>
     )
